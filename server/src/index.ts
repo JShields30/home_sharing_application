24aa188e96a2d836b3ad6ce4d4d5080997ee7380
Joe Shields <shields.john.joseph@gmail.com>
2021-02-03 16:44:14 -0600

    Made number consts strictly defined

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,8 @@ import express from 'express';
 const app = express();
 const port = 9000;
 
-const one = 1;
-let two = 2;
-
-two = "two";
+const one: number = 1;
+const two: number = 2;
 
 app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
 
