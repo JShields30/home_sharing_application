5221af5a3cefadc045b353b560c62b4343e744dd
Joe Shields <shields.john.joseph@gmail.com>
2021-03-01 20:26:51 -0600

    added body parser and post requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { listings } from "./listings";
 
 const app = express();
 const port = 9000;
+
+app.use(bodyParser.json())
 
 const one = 1;
 const two = 2;
@@ -11,6 +14,18 @@ app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
 
 app.get("/listings", (_req, res) => {
   return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send("Failed to delete listing")
 });
 
 app.listen(port);
