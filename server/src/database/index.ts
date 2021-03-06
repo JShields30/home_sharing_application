ce2db5adbf43755e6fe2602264102705a6a2f880
Joe Shields <shields.john.joseph@gmail.com>
2021-03-05 21:18:46 -0600

    stashing

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,8 +1,10 @@
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USERPASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`;
-
+import * as dotenv from "dotenv";
+dotenv.config();
+import { Listing } from '../listings'
+​
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`;
+​
 export const connectDatabase = async () => {
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
@@ -11,6 +13,17 @@ export const connectDatabase = async () => {
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings")
+    listings: db.collection<Listing>("listings_2")
   };
 };
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user_001:<password>@cluster0.ocs2m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,28 @@
-import express, { Application } from "express";
-import { ApolloServer } from "apollo-server-express";
-import { connectDatabase } from "./database";
-import { typeDefs, resolvers } from "./graphql";
 
-const port = 9000;
-
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express, { Application } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from './database';
+import { typeDefs, resolvers } from './graphql';
+​
+const PORT = 9000;
+​
 const mount = async (app: Application) => {
-  const db = await connectDatabase();
-  app.use()
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => ({ db })
-  });
-  server.applyMiddleware({ app, path: "/api" });
-
-  app.listen(port);
-
-  console.log(`[app]: http://localhost:${port}`)
-
-  const listings = await db.listings.find({}).toArray();
-  console.log(listings);
+	const db = await connectDatabase();
+	app.use(express.json({ limit: '2mb' }));
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: (req: Request, res: Response) => ({ req, res, db })
+	});
+	server.applyMiddleware({ app, path: '/api' });
+​
+	app.listen(PORT);
+​
+	console.log(`[app]: http://localhost:${process.env.PORT ?? PORT}`);
+​
+	const listings = await db.listings.find({}).toArray();
+	console.log(listings);
 };
-
-mount(express());
+​
