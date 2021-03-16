77abed25c5e4a17d19876f352cc960ba273ef61e
Joe Shields <shields.john.joseph@gmail.com>
2021-03-15 19:50:33 -0500

    set up listings resolvers for id and delete config

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,20 +1,22 @@
-import { IResolvers } from "apollo-server-express";
-import { listings } from "../listings";
+import { ObjectId } from "mongodb";
+import { defaultCreateRemoteResolver, IResolvers } from "apollo-server-express";
+import { Database } from "../lib/types";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+      return await db.listings.find({}).toArray();
     }
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (_root: undefined, { id }: { id: string }, { db }: { db: Database }) => {
+      const deleteRes = await db.listings.findOneAndDelete({_id: new ObjectId(id)});
+
+      if (!deleteRes.value) {
+        throw new Error("Failed to delete listing")
       }
-      throw new Error("Failed to deleted listing");
+
+      return deleteRes.value;
     }
   }
 };
